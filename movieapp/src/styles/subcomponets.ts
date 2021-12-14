2d46025930d627d9c16f76a19db938374dc28778
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2021-12-13 23:08:51 -0500

    added styles to achieve rounded corners

--- movieapp/src/styles/subcomponets.ts
+++ movieapp/src/styles/subcomponets.ts
@@ -3,19 +3,22 @@ import styled from "styled-components";
 export const MoviesList = styled.div`
   display: flex;
   flex-wrap: wrap;
+  background-color: black;
 `;
 
 export const MovieContainer = styled.div`
-  border: 1px solid black;
-  width: 15%;
+  // border: 1px solid black;
+  width: 250px;
   height: fit-content;
   margin: 10px;
+  background-color: white;
+  border-radius: 15px;
 `;
 
 export const Image = styled.img`
   width: 100%;
   height: 100%;
-  border: 1px solid red;
+  border-radius: 15px 15px 0px 0px;
 `;
 
 export const TitleAndRating = styled.div`
@@ -23,5 +26,5 @@ export const TitleAndRating = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  border: 1px solid red;
+  // border: 1px solid red;
 `;
